89e9a071ce6dcc1a96b8df94939d7030dec16488
smorin8674 <sarahmorin@gwu.edu>
2018-09-13 21:57:50 -0400

    updates to 12 and 15

--- ques12.c
+++ ques12.c
@@ -2,22 +2,24 @@
 
 /* question 12 */
 int ques12(int x, int m, int n) {
-    int a = ~m+1;
-    int b = ~x +1;
-    a = x + a;
-    b = b + n;
+    int a = ~m+1;	// Set a to 2's complement of m or -m
+    int b = ~x +1;	// Set b to 2's complement of n or -n
+    // Compare x to m and n
+    a = x + a;		// Set a to x + a or a-n 
+    b = b + n;		// Set b to b + n or n-x
 
-    return !((a|b) >> 31);
+    // If x is between m and n return 1, otherwise return 0
+    return !((a|b) >> 31);	// Return the inverse of the sign bit of bitwise a or b 
 }
-//if x>m and x<n return 1, else return 0
 
 int ans12 (int x, int m, int n) {
-	if(x>m and x<n){
+	if(x>m && x<n){
 		return 1;
 	}
 	return 0;
 }
 
 int main () {
-	
+	printf("ques12 ")
+
 }
--- ques15.c
+++ ques15.c
@@ -5,14 +5,16 @@
 int ques15(int x, int n) {
     /* for ques15 only, assume n is not a negative number  */
 
-    int temp = (1 << n);
-    int z = temp + ~0;
+    int temp = (1 << n);	// Set temp to 2^n
+    int z = temp + ~0;		// Set z to temp + 1 or 2^n + 1
 
-    return (z & x);
+    // if x is zero or x has zeroes in the 1 and n places, return 0, otherwise return 1
+    return (z & x);			//return bitwise and of z nd x
 }
 
 int ans15(int x, int n) {
-	
+	int z = 2**n + 1;
+	return (z & x);
 }
 
 int main () {
