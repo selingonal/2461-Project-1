d9e8c7724443669ba130fb45c53cf00dfdb862e7
smorin8674 <sarahmorin@gwu.edu>
2018-09-16 13:59:13 -0400

    ques9

--- ques9.c
+++ ques9.c
@@ -5,20 +5,26 @@
 int ques9(int x, int n, int c) {
     int n8 = n << 3;			// Set n8 eqaul to 8n (byte shift)
     //255 largest 8 bit number, shifting by 8 bits (1 byte) at a time
-    int mask = 0xff << n8;		// Set mask equal to 255 << 8n, if n>4, mask = 0, if n = 0 you get 255*2
-    int cshift = c << n8;		// Set cshift equal to c << 8n, if n>4 cshift = 0, 
-    int z= (x & ~mask);			// Get everything but 8 bits (location determined by n) of x i.e. get 3/4 bytes of x
+    int mask = 0xff << n8;		// Set mask equal to 255 shifted to the nth byte
+    int cshift = c << n8;		// Set cshift equal to c shifted to the nth byte
+    int z= (x & ~mask);			// Get everything but the nth byte of x
 
-    return (z  | cshift);
+    return (z  | cshift);		//Replace the nth byte in x with the nth byte in c
 }
 
 int ans9(int x, int n, int c) {
-	return 0;
+	int byte = 8*n;
+	int mask = 255 << byte;
+	int new_byte = c << byte;
+	return (new_byte | (x & ~mask));
 }
 
 int main () {
-	printf("ques9(%d, %d, %d): %d\n", 0,0,0,ques9(0,0,0));
-	printf("ques9(%d, %d, %d): %d\n", 1,1,1,ques9(1,1,1));
-	printf("ques9(%d, %d, %d): %d\n", 4,4,12,ques9(4,4,12));
-	printf("ques9(%d, %d, %d): %d\n", 8,9,10,ques9(8,9,10));
+	int x;
+	int n;
+	int c;
+	printf("Enter three integers: ");
+	scanf("%d %d %d", &x, &n, &c);
+	printf("ques9(%d, %d, %d) replaces the nth byte in the %d with the nth byte in %d, giving %d", x,n,c,x,c,ques9(x,n,c));
+	printf("ans9(%d, %d, %d) replaces the nth byte in the %d with the nth byte in %d, giving %d", x,n,c,x,c,ans9(x,n,c));
 }
