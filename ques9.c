5f4d8dafa037ee977f44712418c80bd5be250ded
smorin8674 <sarahmorin@gwu.edu>
2018-09-16 11:55:10 -0400

    ques9

--- ques9.c
+++ ques9.c
@@ -3,8 +3,8 @@
 /* question 9 */
 
 int ques9(int x, int n, int c) {
-    int n8 = n << 3;
-    int mask = 0xff << n8;
+    int n8 = n << 3;			// Set n8 eqaul to 8n
+    int mask = 0xff << n8;		// Set mask equal to 255 << 8n, if n>4, mask = 0, if n = 0 you get 255*2
     int cshift = c << n8;
     int z= (x & ~mask);
 
@@ -16,5 +16,12 @@ int ans9(int x, int n, int c) {
 }
 
 int main () {
-	
+	printf("ques9(%d, %d, %d): %d\n", 0,0,0,ques9(0,0,0));
+	printf("ques9(%d, %d, %d): %d\n", 1,1,1,ques9(1,1,1));
+	printf("ques9(%d, %d, %d): %d\n", 4,4,12,ques9(4,4,12));
+	printf("ques9(%d, %d, %d): %d\n", 8,9,10,ques9(8.9.10));
+
+
+
+
 }
