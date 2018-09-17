234e41a14b372382f3fc8d2bf82f39a4926cf1b8
tuhinadasgupta <32200009+tuhinadasgupta@users.noreply.github.com>
2018-09-16 20:42:51 -0400

    Update ques1.c

--- ques1.c
+++ ques1.c
@@ -2,14 +2,22 @@ int ques1(int x) {
     int z;
     int y  = ~x+1; //y is the 2's complement of x 
     z= (~(y|x)); // OR of original and its 2's complement then take the complement 
-    z = (z >> 31);
+    z = (z >> 31); //shifted right 31 bits 
 
-    return (z & 1);
+    return (z & 1); // only if x is 0 will this AND operation be true and return 1 
 
 }
 /* Answer to PART B: */
-// the function always returns zero regardless of what the input is 
+// the function returns zero so long as the input isn't zero. In that case, it returns 1 
 int ques1(int x){
-    x=0; 
-    return x; 
+    int y; 
+    if (x==0)
+    {
+     y =1; 
+    }
+    else
+    {
+      y =0; 
+    }
+    return y; 
 }
