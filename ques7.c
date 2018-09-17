e5db5be2e93154f0e09d2d677db76d31d0c76561
tuhinadasgupta <32200009+tuhinadasgupta@users.noreply.github.com>
2018-09-17 15:50:03 -0400

    Update ques7.c

--- ques7.c
+++ ques7.c
@@ -6,6 +6,61 @@ int ques7(int x) {
 //if x is even, the rightmost bit is returned 
 //if x is odd, 1 is returned 
 
+
+
+int ques7(int x) {
+
+    return x & (~x+1);
+}
+
+/* Answer to Part B */ 
+int getRightOneBitPosition(int x)
+{
+    return log2(x&-x)+1; //power to which 2 should be raised
+}
+int exponential(int y)
+{
+    int result=0;
+    for (int i=1; i<y; i++)
+    {
+        result+= 2 * 2;
+    }
+    if (y==1)
+    {
+        result =2; 
+    }
+    return result; // the value at the correct location
+}
+int ans7(int y, int x){
+    int k;
+    //odd
+    if (x % 2 == 1) {
+        k =1;
+    }
+    else{
+        k = exponential(y);
+    }
+    return k;
+}
+
+int main()
+{
+    int x;
+    x = 6;
+    int y;
+    y = getRightOneBitPosition(x);
+    if (y !=1)
+    {
+     y = y-1;
+    }
+    int z;
+    int q;
+    q = ques7(x);
+    z = ans7(y,x);
+    printf("%d\n", q);
+    printf("%d", z);
+}
+
 int getRightOneBitPosition(int x)
 {
     return log2(x&-x)+1; //power to which 2 should be raised
